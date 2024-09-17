6eeb191dcd729eeb64317702e74698d5352e77f3
Adida <adida.barack@student.moringaschool.com>
2024-09-17 19:25:33 +0300

    new models and tables added plus the first migration for testing

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 import re
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, DateTime
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, request, jsonify
 
@@ -61,6 +62,62 @@ class User(db.Model):
             raise ValueError("Invalid Phone Number Format!")
         if not self.validate_password(password):
             raise ValueError("Invalid Password Format!")
+        
+class Product(db.Model):
+    __tablename__ = "products"
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, unique=True, nullable=False)
+    sku=db.Column(db.String, unique=True, nullable=False)
+    description=db.Colum(db.String, nullable=False)
+    price=db.Column(db.Float, nullable=False)
+    quantity_in_stock=db.Column(db.Integer, nullable=False)
+    supplier_id=db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return{
+            "id":self.id,
+            "name":self.name,
+            "sku":self.sku,
+            "description":self.description,
+            "price":self.price,
+            "quantity_in_stock":self.quantity_in_stock,
+            "supplier_id":self.supplier_id
+        }
+    
+class Supplier(db.Model):
+    __tablename__ = "suppliers"
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, unique=True, nullable=False)
+    contact=db.column(db.string, unique=True, nullable=False)
+
+    def to_dict(self):
+        return{
+            "id":self.id,
+            "name":self.name,
+            "contact":self.contact
+        }
+    
+class Sale(db.Model):
+    __tablename__ = "sales"
+    id=db.Column(db.Integer, primary_key=True)
+    product_id=db.Column(db.Integer, nullable=False)
+    name=db.Calumn(db.String, nullable=False)
+    quantity_sold=db.Column(db.Integer, nullable=False)
+    total_price=db.Column(db.Float, nullable=False)
+    date_of_sale=db.Column(DateTime, default=datetime.utcnow, nullable=False)
+    receipt_id=db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        {
+            "id":self.id,
+            "product_id":self.product_id,
+            "name":self.name,
+            "quantity_sold":self.quantity_sold,
+            "total_price":self.total_price,
+            "date_of_sale":self.date_of_sale,
+            "receipt_id":self.receipt_id
+        }
+
 
     
 
